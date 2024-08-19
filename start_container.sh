c101925f2954879aa0e012215643125e153037fd
Antony-Smiledan <160041215+Antony-Smiledan@users.noreply.github.com>
2024-08-19 13:02:27 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -e
 
+# PULL THE DOCKER IMAGE FROM DOCKER HUB
+docker pull smiledan/ci-cd:latest
+
+# RUN THE DOCKER IMAGE AS A CONTAINER
+docker run -itd -p 5000:5000 smiledan/ci-cd:latest
